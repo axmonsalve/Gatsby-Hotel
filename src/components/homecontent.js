460af3543247d09156dccc95396aafcb79b10a20
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-02-08 22:10:11 -0300

    Css to main section

--- src/components/homecontent.js
+++ src/components/homecontent.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import { css } from '@emotion/core';
+
+const HomeText = styled.div`
+    padding-top: 4rem;
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+
+    @media (min-width: 768px){
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        column-gap: 2 rem;
+    }
+
+    p{
+        line-height: 2;
+    }
+`;
 
 const HomeContent = () => {
 
@@ -27,11 +46,17 @@ const HomeContent = () => {
 
     return ( 
         <>
-            <h2>{titulo}</h2>
-            <div>
+            <h2
+                css={css`
+                    text-align: center;
+                    font-size: 4rem;
+                    margin-top: 4rem;
+                `}
+            >{titulo}</h2>
+            <HomeText>
                 <p>{contenido}</p>
                 <Image fluid={imagen.fluid} alt=""/>
-            </div>
+            </HomeText>
         </>
      );
 }

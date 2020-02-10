23ba49b2b300e5a6a6ba568da58898983adac134
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-02-10 14:17:23 -0300

    Creating dynamic pages with programming

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,29 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const result = await graphql(`
+        query{
+            allDatoCmsHabitacion{
+                nodes{
+                    slug
+                    }
+            }
+        }
+    `);
 
-// You can delete this file if you're not using it
+    //console.log(result.data.allDatoCmsHabitacion.nodes)
+
+    if (result.errors){
+        reporter.panic('No hubo resultados', result.errors);
+    }
+
+    //Si hay paginas, crear los archivos
+    const rooms = result.data.allDatoCmsHabitacion.nodes;
+    rooms.forEach(room => {
+        actions.createPage({
+            path: room.slug,
+            component: require.resolve('./src/components/rooms.js'),
+            context: {
+                slug: rooms.slug
+            }
+        })
+    });
+}
